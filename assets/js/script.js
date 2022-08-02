8de0525917b60a4b956c8adb1c83665916faeaa6
Mykneeisonfire <108244837+Mykneeisonfire@users.noreply.github.com>
2022-08-01 20:26:55 -0400

    index and questions

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        question: "Javascript is an _______ language?",
+        choices: ["a. <object-orientedt>", "b. <object-based>", "c. <procedural>", "d. <none of the above>"],
+        answer: "a. <object-orientedt>"
+    },
+    {
+        question: "What keyword is used to check whether a given property is valid or not?",
+        choices: ["a. in", "b. is in", "c. exists", "d. lies"],
+        answer: "a. in"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: ["a. numbers and strings", "b. other arrays", "c. booleans", "d. all of the above"],
+        answer: "b. other arrays"
+    },
+    {
+        question: "What are variables used for in JavaScript Programs?",
+        choices: ["a. storing numbers, dates, or other values", "b. varying randomly", "c. causing high-school algebra flashbacks", "d.  none of the above"],
+        answer: "c. alerts"
+    },
+    {
+        question: "To see if two variables are equal in an if / else statement you would use ____.",
+        choices: ["a. =", "b. ==", "c. 'equals'", "d. !="],
+        answer: "b. =="
+    },
+    {
+        question: "The first index of an array is ____.",
+        choices: ["a. 0", "b. 1", "c. 8", "d. any"],
+        answer: "a. 0"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["a. if i == x then", "b. if i = x then", "c. if(i == x)", "d. if i = x"],
+        answer: "c. if(i == x)"
+    },
+    {
+        question: "How do you add a comment in a JavaScript?",
+        choices: ["a. //This is a comment", "b. <!--This is a comment-->", "c. 'This is a comment", "d. * This is a comment *"],
+        answer: "a. //This is a comment"
+    },
+]
